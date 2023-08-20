4f789550c5c1659b44a31b76a7552c6de78b4524
AdnanSakib04 <navidadnan04@gmail.com>
2023-08-20 18:05:01 +0600

    cart updated

--- js/cart-and-coupon.js
+++ js/cart-and-coupon.js
@@ -1,9 +1,8 @@
 function addToTotal(target) {
-    document.getElementById('btn-purchase').removeAttribute('disabled');
+   
     
     console.log(target.parentNode.childNodes[1].childNodes[3].childNodes);
-    // console.log(target.parentNode.childNodes[3].childNodes[3].childNodes[3].innerText);
-    // console.log(target.parentNode.childNodes[3].childNodes[3].childNodes[5].innerText.split(" ")[0]);
+    
     const nameOfItem = target.parentNode.childNodes[1].childNodes[3].childNodes[3].innerText
     const priceOfItemString = target.parentNode.childNodes[1].childNodes[3].childNodes[5].innerText.split(" ")[0];
     const priceOfItem = parseFloat(priceOfItemString);
@@ -13,6 +12,7 @@ function addToTotal(target) {
     const totalPriceElement = document.getElementById('total-price');
     const previousTotalPriceString = totalPriceElement.innerText;
     const previousTotalPrice = parseFloat(previousTotalPriceString);
+
 
     // get the previous total price with discount
     const totalElement = document.getElementById('total')
@@ -34,6 +34,9 @@ function addToTotal(target) {
     //set the total price with discount
     totalElement.innerText = newTotal;
 
+    
+    checkApplyAndPurchaseButton(newTotalPrice);
+
     //set the name of the product 
     const olElement = document.getElementById('order-list');
     const li = document.createElement('li');
@@ -42,35 +45,29 @@ function addToTotal(target) {
 
 }
 
-// attempt to solve the make purchase button
-// document.getElementById('total-price').addEventListener('input', function(){
-//     const totalPriceElement = document.getElementById('total-price');
-//     const previousTotalPriceString = totalPriceElement.innerText;
-//     const previousTotalPrice = parseFloat(previousTotalPriceString);
-//     const btnPurchase = document.getElementById('btn-purchase');
+function checkApplyAndPurchaseButton(newTotalPrice){
+    if(newTotalPrice>0)
+    {
+        document.getElementById('btn-purchase').removeAttribute('disabled');
+    }
+    if(newTotalPrice>=200)
+    {
+        document.getElementById('btn-apply').removeAttribute('disabled');
+    }
 
-//             if(previousTotalPrice>0){
-//                 btnPurchase.removeAttribute('disabled');
-//             }else {
-//                 btnPurchase.setAttribute('disabled', true);
-//             }
-// })
+}
+
+
     
 
-
-document.getElementById('coupon-field').addEventListener('keyup', function (event) {
-    const couponCode = event.target.value;
-    const btnApply = document.getElementById('btn-apply');
-    console.log(couponCode);
-
-    if (couponCode === 'SELL200') {
-        btnApply.removeAttribute('disabled');
-    }
-    else {
-        btnApply.setAttribute('disabled', true);
-    }})
-
 function discount() {
+
+    const couponCode = document.getElementById('coupon-field').value;
+    if (couponCode != 'SELL200') {
+    alert('Invalid coupon');
+    }
+    else{
+
         // get the previous total price without discount
         const totalPriceElement = document.getElementById('total-price');
         const previousTotalPriceString = totalPriceElement.innerText;
@@ -83,4 +80,5 @@ function discount() {
         const newTotal = (previousTotalPrice - discountAmount).toFixed(2);
         const totalElement = document.getElementById('total')
         totalElement.innerText = newTotal;
+    }
     }
